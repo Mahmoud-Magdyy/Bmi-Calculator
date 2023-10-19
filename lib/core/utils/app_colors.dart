5737f552c7092efda43bd671748dd7f34e8e901c
Mahmoud <mahmoudmagdy20002@gmail.com>
2023-10-19 18:47:12 +0300

    finish project

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor{ 
+// var  kBottomContainerColour = const Color(0xFFEB1555);
+  static const bodyColor =  Color(0xFF0A0E21);
+static const containerColor =  Color(0xFF1A1F38);
+static const textColor = Colors.white;
+// static const kActiveCardColour = Color(0xFF1D1E33);
+// static const kInactiveCardColour = Color(0xFF111328);
+
+} 
